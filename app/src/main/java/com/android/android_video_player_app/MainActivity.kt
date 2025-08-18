2a83d6fd797ed264210a937b4bf8972db4fc2ea9
zearkiatos <caprilespe@outlook.com>
2025-08-18 14:20:38 -0400

    Add video player

--- app/src/main/java/com/android/android_video_player_app/MainActivity.kt
+++ app/src/main/java/com/android/android_video_player_app/MainActivity.kt
@@ -20,24 +20,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val uri: Uri = parse("android.resource://" + packageName + "/" + "test")
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        val videoView = findViewById<VideoView>(binding.videoView.id)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        val uri:Uri = parse("android.resource://$packageName/raw/test")
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
     }
 
 }
